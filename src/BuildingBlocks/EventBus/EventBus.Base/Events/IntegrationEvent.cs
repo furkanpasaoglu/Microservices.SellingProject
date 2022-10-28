29a22b02b7ed6531d28affd9a226b65cb0aa5881
furkan paşaoğlu <furkan.pasaoglu@hotmail.com>
2022-10-28 17:21:14 +0300

    Added EventBus(RabbitMQ,Azure)

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EventBus.Base.Events;
+
+public class IntegrationEvent
+{
+    [JsonPropertyName("Id")]
+    public Guid Id { get; private set; }
+
+    [JsonPropertyName("CreatedDate")]
+    public DateTime CreatedDate { get; private set; }
+
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreatedDate = DateTime.Now;
+    }
+
+    [JsonConstructor]
+    public IntegrationEvent(Guid id, DateTime createdDate)
+    {
+        Id = id;
+        CreatedDate = createdDate;
+    }
+}
